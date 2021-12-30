1da7fb078aeb2723902935eb7a0a6572f02a57e0
moonthecoder <94987124+moonthecoder@users.noreply.github.com>
2021-12-30 22:18:19 +0100

    Update HavenHubV4.1.lua

--- HavenHubV4.1.lua
+++ HavenHubV4.1.lua
@@ -1,6 +1,7 @@
--- https://discord.io/bittersweet
-
-game:GetService("StarterGui"):SetCore("SendNotification",{Title="Outdated Version",Text="The new script for v4.2 is in our discord, a invite url has been copied or just go to discord.io/bittersweet",Button1="Ok!",Duration=30000000})
-setclipboard("https://discord.gg/bEB3GAqThV")
--- Russian version. 
-game:GetService("StarterGui"):SetCore("SendNotification",{Title="Outdated Version",Text="Этот хаб старый, присоединяйтесь к нашему разногласию, чтобы получить новый, он скопирован.",Button1="👍",Duration=30000000})
+setclipboard("https://discord.gg/8XZc6unhmv")
+game:GetService("StarterGui"):SetCore("SendNotification",{     
+Title = "Outdated",     
+Text = "Hello! You are currently using an outdated version of Haven Hub, We have copied the server invite link to your clipboard. Join it and get the script from there.";
+Button1 = "Ok!",
+Duration = 30
+})
